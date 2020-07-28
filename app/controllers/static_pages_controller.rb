c6711801f0a30f70c302e19e6773ff9cecfb3a01
daisaku <d_sawada@hotmail.co.jp>
2020-07-28 23:08:39 +0900

    ログイン画面完成

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,21 @@
 class StaticPagesController < ApplicationController
+  before_action :logged_in_user, only: [:top]
+
   def top
   end
 
   def show
+  end
+end
+
+private
+
+# beforeフィルター
+
+# ログイン済みのユーザーか確認します。
+def logged_in_user
+  if user_signed_in?    
+    flash[:success] = "ログインしました"
+    redirect_to static_pages_show_path
   end
 end
